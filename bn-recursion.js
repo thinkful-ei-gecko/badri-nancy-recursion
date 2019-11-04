245b277672b26e2447a1829cf7f14dca1bbf624d
nphiukhao <nphiukhao@gmail.com>
2019-11-04 13:13:18 -0800

    up till fib

--- bn-recursion.js
+++ bn-recursion.js
@@ -1,9 +1,9 @@
 
 ///counting sheep 
-const countSheep = function(count) {
-    if(count === 0) {
+const countSheep = function (count) {
+    if (count === 0) {
         console.log('All sheep jumped over the fence')
-        return ;
+        return;
     }
     console.log(count + ': Another sheep jumps over the fence')
     countSheep(count - 1)
@@ -12,7 +12,7 @@ const countSheep = function(count) {
 //countSheep(3)
 
 //power calculator
-const powerCalculator = function(base, exponent) {
+const powerCalculator = function (base, exponent) {
     if (exponent < 0) {
         console.log('exponent must be greater than 0')
         return;
@@ -20,8 +20,58 @@ const powerCalculator = function(base, exponent) {
     if (exponent === 1) {
         return base
     }
-    
-    return base * powerCalculator(base, exponent-1)
+
+    return base * powerCalculator(base, exponent - 1)
+}
+//console.log(powerCalculator(3, 4))
+
+const reverseString = function (string) {
+    if (string === '') {
+        return '';
+    }
+
+    const newChar = string[string.length - 1]
+
+    return newChar + reverseString(string.substring(0, string.length - 1)
+
+    )
+}
+//console.log(reverseString('hello'))
+
+const triNum = function (n) {
+    if(n === 1) {
+        return 1;
+    }
+    return n + triNum(n-1)
 }
 
-console.log(powerCalculator(3, 4))
+//console.log(triNum(5))
+
+const strSplit = function (string, sep) {
+
+    let index = string.indexOf(sep)
+    if (index === -1) {
+        return [string] //2020
+    }
+
+    return [string.slice(0, index)].concat(strSplit(string.slice(index + sep.length), sep))
+    
+}
+
+//console.log(strSplit('02/20/2020', '/')) 
+
+
+
+
+const fibMe = function(n) {
+
+    if(num === 1) {
+        return 1;
+    }
+    
+    return num 
+}
+
+
+//input 7
+//output 1, 1, 2, 3, 5, 8, 13.
