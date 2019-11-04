def5140b99432b8d42d380219960532e9910717f
Badri-narayan <badritulsiram@gmail.com>
2019-11-04 11:07:32 -0800

    power calc

--- bn-recursion.js
+++ bn-recursion.js
@@ -9,4 +9,19 @@ const countSheep = function(count) {
     countSheep(count - 1)
 }
 
-countSheep(3)
+//countSheep(3)
+
+//power calculator
+const powerCalculator = function(base, exponent) {
+    if (exponent < 0) {
+        console.log('exponent must be greater than 0')
+        return;
+    }
+    if (exponent === 1) {
+        return base
+    }
+    
+    return base * powerCalculator(base, exponent-1)
+}
+
+console.log(powerCalculator(3, 4))
